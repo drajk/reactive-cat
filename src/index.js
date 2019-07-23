c856c111768d55026827338bb33181d745dc72be
Dheeraj Khushalani <dheerajkhs@gmail.com>
2019-07-23 21:22:40 +1000

    feat: initial commits with variables and clean-up

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+import './variables.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './layouts/Home/Home';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
---
+++ src/layouts/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+function Home() {
+  return (
+    <div className={styles.app}>
+      <header className={styles.header}>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
